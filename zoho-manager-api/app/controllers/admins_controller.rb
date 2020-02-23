289105f82b1cae94059c7079e96fe9971c6e2b6d
Juan D. Bermudez <juan.bermudez1102@outlook.com>
2020-02-23 14:18:48 -0500

    Added create method for admins controller

--- zoho-manager-api/app/controllers/admins_controller.rb
+++ zoho-manager-api/app/controllers/admins_controller.rb
@@ -1,2 +1,14 @@
 class AdminsController < ApplicationController
+  def create
+    admin = Admin.new
+    account = admin.build_account(admin_params)
+    if account.save
+      render json: admin
+    end
+  end
+
+  private
+  def admin_params
+    params.require(:admin).permit(:name,:email,:password)
+  end
 end

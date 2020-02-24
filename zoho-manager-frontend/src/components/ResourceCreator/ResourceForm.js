64bfde2f626ff2017bb80c1ff9234c49ffd09de9
Juan D. Bermudez <juan.bermudez1102@outlook.com>
2020-02-24 14:38:11 -0500

    Generated ResourceForm component

---
+++ zoho-manager-frontend/src/components/ResourceCreator/ResourceForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class ResourceForm extends Component {
+  render() {
+    return (
+      <>
+      
+      </>
+    );
+  }
+}
+
+export default ResourceForm;

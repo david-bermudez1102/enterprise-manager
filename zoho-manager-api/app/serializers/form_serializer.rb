17e662f8f9e439ecee3af1cb2dfbb8ecfadcd5b0
Juan D. Bermudez <juan.bermudez1102@outlook.com>
2020-02-25 18:52:34 -0500

    Removed fields from form serializer

--- zoho-manager-api/app/serializers/form_serializer.rb
+++ zoho-manager-api/app/serializers/form_serializer.rb
@@ -1,5 +1,4 @@
 class FormSerializer
   include FastJsonapi::ObjectSerializer
   attributes :id, :name, :organization_id
-  has_many :fields
 end
--- zoho-manager-frontend/src/components/Fields/Field.js
+++ zoho-manager-frontend/src/components/Fields/Field.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Field = ({ field }) => {
-  return <div><input type={field.fieldType} name={field.name}/></div>
+  return <div><input type={field.fieldType} name={field.name} placeholder={`Enter ${field.name}`}/></div>
 };
 
 export default Field;

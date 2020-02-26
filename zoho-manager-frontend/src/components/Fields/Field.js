cc979b47b59707ea06aa9f1753b5f3d94e9c01cb
Juan D. Bermudez <juan.bermudez1102@outlook.com>
2020-02-25 20:59:19 -0500

    Bug fixes in form

--- zoho-manager-frontend/src/components/Fields/Field.js
+++ zoho-manager-frontend/src/components/Fields/Field.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 
 class Field extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = { value: "" };
   }
 
--- zoho-manager-frontend/src/components/Fields/FieldsList.js
+++ zoho-manager-frontend/src/components/Fields/FieldsList.js
@@ -4,19 +4,22 @@ import Field from "./Field";
 const pluralize = require("pluralize");
 
 class FieldsList extends Component {
-  constructor(){
-    super()
+  constructor() {
+    super();
+    this.state = {};
   }
+
+  handleSubmit = event => {
+    event.preventDefault();
+  };
 
   render() {
     const { fields, resource } = this.props;
     return (
-      <form>
-        <div>
-          {fields.map(field => (
-            <Field field={field} />
-          ))}
-        </div>
+      <form onSubmit={this.handleSubmit}>
+        {fields.map(field => (
+          <Field key={field.id} field={field} />
+        ))}
         <input
           type="submit"
           value={`Create ${pluralize.singular(resource.name)}`}

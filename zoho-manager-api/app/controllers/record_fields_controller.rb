f54efae3bf9d5c9e4e6f067ce54b78cbb4672642
Juan D. Bermudez <juan.bermudez1102@outlook.com>
2020-03-03 16:29:23 -0500

    Added update and destroy methods for recordFields

--- zoho-manager-api/app/controllers/record_fields_controller.rb
+++ zoho-manager-api/app/controllers/record_fields_controller.rb
@@ -6,7 +6,24 @@ class RecordFieldsController < ApplicationController
     render json: RecordFieldSerializer.new(record_fields)
   end
 
+  def update
+    record_field = @form.record_fields.find_by(id: params[:id])
+    render json: RecordFieldSerializer.new(record_field) if record_field.update(record_field_params)
+  end
+
+  def destroy
+    record_field = @form.record_fields.find_by(id: params[:id])
+    render json: { id: params[:id], message: 'Success' } if record_field.destroy
+  end
+
+
   private
+  def record_field_params
+    params.require(:record_field).permit(
+      :name,
+      :form_id
+    )
+  end
 
   def set_form
     @form = Form.find(params[:form_id])

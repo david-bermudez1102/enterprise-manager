73291c2fd9d58b476d2499f271b8891b1fedd31e
Juan D. Bermudez <juan.bermudez1102@outlook.com>
2020-03-09 15:21:23 -0400

    Added create action

--- zoho-manager-api/app/controllers/employees_controller.rb
+++ zoho-manager-api/app/controllers/employees_controller.rb
@@ -7,7 +7,18 @@ class EmployeesController < ApplicationController
     render json: EmployeeSerializer.new(employees)
   end
 
+  def create
+    employee = @admin.employees.new
+    one_time_password = SecureRandom.hex
+    account = employee.build_account(employee_params, password: one_time_password)
+    render json: EmployeeSerializer.new(employee, one_time_password: one_time_password) if account.save
+  end
+
   private
+    def employee_params
+      params.require(:employee).permit(:name, :email)
+    end
+
     def set_admin
       @admin = Admin.find(params[:admin_id])
     end
--- zoho-manager-api/app/controllers/managers_controller.rb
+++ zoho-manager-api/app/controllers/managers_controller.rb
@@ -6,7 +6,18 @@ class ManagersController < ApplicationController
     render json: ManagerSerializer.new(managers)
   end
 
+  def create
+    manager = @admin.managers.new
+    one_time_password = SecureRandom.hex
+    account = manager.build_account(manager_params, password: one_time_password)
+    render json: ManagerSerializer.new(manager, one_time_password: one_time_password) if account.save
+  end
+
   private
+    def manager_params
+      params.require(:manager).permit(:name, :email)
+    end
+
     def set_admin
       @admin = Admin.find(params[:admin_id])
     end

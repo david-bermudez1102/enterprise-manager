0a8c51bb7081d048d86dc32734e1e6b146f8bbdb
Juan D. Bermudez <juan.bermudez1102@outlook.com>
2020-03-09 15:03:58 -0400

    Added reducer for accounts

---
+++ zoho-manager-frontend/src/reducers/accountsReducer.js
@@ -0,0 +1,42 @@
+export const accountsReducer = (state = [], action) => {
+  switch (action.type) {
+    case "ADD_EMPLOYEE":
+      return [...state, action.employee];
+    case "FETCH_EMPLOYEES":
+      return [
+        ...state,
+        ...action.employees.filter(
+          employee => !state.some(e => employee.id === e.id)
+        )
+      ];
+    case "UPDATE_EMPLOYEE":
+      const employee = state.find(
+        employee => employee.id === parseInt(action.employeeId)
+      );
+      return [...state.map(e => (e.id === employee.id ? action.employee : e))];
+    case "REMOVE_EMPLOYEE":
+      return [
+        ...state.filter(employee => employee.id !== parseInt(action.employeeId))
+      ];
+    case "ADD_MANAGER":
+      return [...state, action.manager];
+    case "FETCH_MANAGERS":
+      return [
+        ...state,
+        ...action.managers.filter(
+          manager => !state.some(m => manager.id === m.id)
+        )
+      ];
+    case "UPDATE_MANAGER":
+      const manager = state.find(
+        manager => manager.id === parseInt(action.managerId)
+      );
+      return [...state.map(m => (m.id === manager.id ? action.manager : m))];
+    case "REMOVE_MANAGER":
+      return [
+        ...state.filter(manager => manager.id !== parseInt(action.managerId))
+      ];
+    default:
+      return state;
+  }
+};

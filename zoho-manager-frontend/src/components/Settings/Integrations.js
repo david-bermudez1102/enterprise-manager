e5835b54a141e8d6ba107878d9788ffd370d399e
Juan D. Bermudez <juan.bermudez1102@outlook.com>
2020-03-11 12:23:32 -0400

    Added nested routing and basic buttons prototype

--- zoho-manager-frontend/src/components/Settings/Integrations.js
+++ zoho-manager-frontend/src/components/Settings/Integrations.js
@@ -3,11 +3,10 @@ import React, { Component } from "react";
 class Integrations extends Component {
   render() {
     return (
-      <div className="row">
-        <div className="col-lg-5">
-          <div className="list-group"></div>
-        </div>
-        <div className="col-lg-7"></div>
+      <div>
+        Account is not connected to any Accounting Software.
+        <button>Connect with Zoho Books</button>
+        <button>Connect with QuickBooks</button>
       </div>
     );
   }
--- zoho-manager-frontend/src/containers/Settings/Settings.js
+++ zoho-manager-frontend/src/containers/Settings/Settings.js
@@ -1,13 +1,37 @@
 import React, { Component } from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import Privileges from "../../components/Settings/Privileges";
+import Integrations from "../../components/Settings/Integrations";
+import AccountSettings from "../../components/Settings/AccountSettings";
 
 class Settings extends Component {
   render() {
+    const { match } = this.props;
     return (
       <div className="row">
         <div className="col-lg-5">
-          <div className="list-group"></div>
+          <div className="list-group">
+            <Link to={`${match.url}/privileges`}>Privileges</Link>
+            <Link to={`${match.url}/integrations`}>Integrations</Link>
+            <Link to={`${match.url}/account`}>Account Setttings</Link>
+          </div>
         </div>
-        <div className="col-lg-7"></div>
+        <div className="col-lg-7">
+          <Switch>
+            <Route
+              path={`${match.path}/privileges`}
+              render={props => <Privileges {...props} />}
+            />
+            <Route
+              path={`${match.path}/integrations`}
+              render={props => <Integrations {...props} />}
+            />
+            <Route
+              path={`${match.path}/account`}
+              render={props => <AccountSettings {...props} />}
+            />
+          </Switch>
+        </div>
       </div>
     );
   }

3820cdd3df854d8e217f641ccc5caad30f6f7940
Juan D. Bermudez <juan.bermudez1102@outlook.com>
2020-03-13 13:30:49 -0400

    Added ConnectionsList and Connection

---
+++ zoho-manager-frontend/src/components/Connections/Connection.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Connection extends Component {
+  constructor() {}
+
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Connection;
---
+++ zoho-manager-frontend/src/components/Connections/ConnectionsList.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class ConnectionsList extends Component {
+  constructor() {}
+
+  render() {
+    return <div></div>;
+  }
+}
+
+export default ConnectionsList;

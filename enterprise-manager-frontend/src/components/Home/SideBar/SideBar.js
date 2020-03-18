397735218dfc3018d2a6f22344720b1ea4323861
Juan D. Bermudez <juan.bermudez1102@outlook.com>
2020-03-18 19:09:29 -0400

    Bug fixes in sidebar

--- enterprise-manager-frontend/src/components/Home/SideBar/SideBar.js
+++ enterprise-manager-frontend/src/components/Home/SideBar/SideBar.js
@@ -1,61 +1,61 @@
-import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
-import { CSSTransition } from 'react-transition-group';
-import './styles.css';
-import cuid from 'cuid';
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+import { CSSTransition } from "react-transition-group";
+import "./styles.css";
+import cuid from "cuid";
 
 export default class SideBar extends Component {
   constructor(props) {
     super(props);
-    const navLinkClass = 'nav-item nav-link text-light';
+    const navLinkClass = "nav-item nav-link text-light";
     const activePath = props.location.pathname;
     this.state = {
       minimized: false,
       minimizedFromToggle: false,
       links: [
         {
-          path: '/',
+          path: "/",
           exact: true,
           className: navLinkClass,
-          text: 'Home',
-          icon: 'fas fa-home',
-          textClass: 'ml-2',
-          iconMin: 'fas fa-home'
+          text: "Home",
+          icon: "fas fa-home",
+          textClass: "ml-2",
+          iconMin: "fas fa-home"
         },
         {
-          path: '/organizations',
+          path: "/organizations",
           className: navLinkClass,
           exact: true,
-          text: 'Organizations',
-          icon: 'fas fa-briefcase',
-          iconMin: 'fas fa-briefcase',
-          textClass: 'ml-2',
-          levels: ['admin']
+          text: "Organizations",
+          icon: "fas fa-briefcase",
+          iconMin: "fas fa-briefcase",
+          textClass: "ml-2",
+          levels: ["admin"]
         },
         {
           path: `/organizations/${props.organizations[0].id}/records`,
           className: `${navLinkClass}`,
-          text: 'Records',
-          icon: 'fas fa-th-list',
-          iconMin: 'fas fa-th-list',
-          textClass: 'ml-2',
-          levels: ['admin', 'manager', 'employee']
+          text: "Records",
+          icon: "fas fa-th-list",
+          iconMin: "fas fa-th-list",
+          textClass: "ml-2",
+          levels: ["admin", "manager", "employee"]
         },
         {
-          path: '/accounts',
+          path: "/accounts",
           className: navLinkClass,
-          text: 'Accounts',
-          icon: 'fas fa-users-cog mr-n1',
-          iconMin: 'fas fa-users-cog',
-          levels: ['admin'],
-          textClass: 'ml-2',
+          text: "Accounts",
+          icon: "fas fa-users-cog mr-n1",
+          iconMin: "fas fa-users-cog",
+          levels: ["admin"],
+          textClass: "ml-2",
           dropdown: true,
-          status: activePath.includes('/accounts') ? 'open' : 'closed',
+          status: activePath.includes("/accounts") ? "open" : "closed",
           subLinks: [
             {
-              path: '/accounts/add',
-              text: 'Add account',
-              icon: 'fas fa-user-plus',
+              path: "/accounts/add",
+              text: "Add account",
+              icon: "fas fa-user-plus",
               className: `${navLinkClass}`
             }
           ]
@@ -64,16 +64,16 @@ export default class SideBar extends Component {
           path: `/organizations/${props.organizations[0].id}/resources`,
           dropdown: true,
           className: `${navLinkClass}`,
-          text: 'Resources',
-          icon: 'fas fa-layer-group',
-          iconMin: 'fas fa-layer-group',
-          textClass: 'ml-2',
-          levels: ['admin', 'manager', 'employee'],
+          text: "Resources",
+          icon: "fas fa-layer-group",
+          iconMin: "fas fa-layer-group",
+          textClass: "ml-2",
+          levels: ["admin", "manager", "employee"],
           subLinks: [
             {
               path: `/organizations/${props.organizations[0].id}/resources/new`,
-              text: 'New Resource',
-              icon: 'fas fa-layer-plus',
+              text: "New Resource",
+              icon: "fas fa-layer-plus",
               className: `${navLinkClass}`
             }
           ]
@@ -95,8 +95,8 @@ export default class SideBar extends Component {
       links: this.state.links.map(link =>
         link.dropdown
           ? link.id === linkId
-            ? { ...link, status: link.status === 'open' ? 'closed' : 'open' }
-            : { ...link, status: 'closed' }
+            ? { ...link, status: link.status === "open" ? "closed" : "open" }
+            : { ...link, status: "closed" }
           : { ...link }
       )
     });
@@ -124,100 +124,87 @@ export default class SideBar extends Component {
     return (
       <CSSTransition in={!minimized} timeout={40} classNames="slider" appear>
         <div
-          className="py-3 bg-secondary shadow-lg text-light"
-          style={{ fontSize: '16px' }}>
+          className="pt-3 bg-secondary shadow-lg text-light vh-100 sticky-top"
+          style={{ fontSize: "16px" }}>
           <nav
-            className="px-0 py-0 nav nav-dark nav-pills flex-column sticky-top"
-            style={{ zIndex: 999 }}
-            onMouseEnter={this.openSideBar}
-            onMouseLeave={this.closeSideBar}>
-            <div
-              className={`w-100 d-flex align-items-center flex-wrap text-nowrap px-3 justify-content-between`}>
-              <span className="d-flex align-items-center">
-                <i
-                  className={`fas fa-user-circle text-light ${
-                    !minimized ? 'mr-2' : null
-                  }`}
-                  style={{ fontSize: '30px' }}></i>
-                <CSSTransition
-                  in={!minimized}
-                  timeout={50}
-                  classNames="fade"
-                  appear>
-                  <span>{currentUser.name}</span>
-                </CSSTransition>
-              </span>
-            </div>
-            <hr
-              className="mb-0 w-100"
-              style={{ background: 'rgba(0,0,0,0.2)' }}
-            />
-            <div className="px-2 py-2">
-              {links.map(link => (
-                <span key={cuid()}>
-                  <NavLink
-                    exact={link.exact}
-                    to={link.path}
-                    className={`${link.className} d-flex mb-1  justify-content-between text-nowrap`}
-                    activeClassName="bg-info active shadow"
-                    onClick={() => this.toggleDropDown(link.id)}>
-                    <span>
-                      <i className={link.icon}></i>
-                      {!minimized ? (
-                        <CSSTransition
-                          in={!minimized}
-                          timeout={50}
-                          classNames="fade">
-                          <span className={link.textClass}>{link.text}</span>
-                        </CSSTransition>
-                      ) : null}
-                    </span>
-
-                    {link.dropdown && !minimized ? (
-                      link.status === 'open' ? (
-                        <span>
-                          <i className="fas fa-chevron-down"></i>
-                        </span>
-                      ) : (
-                        <span>
-                          <i className="fas fa-chevron-left"></i>
-                        </span>
-                      )
-                    ) : null}
-                  </NavLink>
-                  {link.dropdown && link.status === 'open' && !minimized
-                    ? link.subLinks.map(subLink => (
-                        <NavLink
-                          to={subLink.path}
-                          className={`${subLink.className} mt-1`}
-                          style={{ backgroundColor: 'rgba(0,0,0,0.3)' }}
-                          key={cuid()}
-                          activeClassName="bg-light text-dark active shadow">
-                          <i className={`fas fa-chevron-right mr-3`}></i>
-                          <i className={`${subLink.icon} mr-1`}></i>
-                          {subLink.text}
-                        </NavLink>
-                      ))
-                    : null}
+            className="px-0 py-0 nav nav-dark nav-pills flex-column h-100 d-flex"
+            style={{ zIndex: 999 }}>
+            <div className="" onMouseEnter={this.openSideBar} onMouseLeave={this.closeSideBar}>
+              <div
+                className={`w-100 d-flex align-items-center flex-wrap text-nowrap px-3 justify-content-between`}>
+                <span className="d-flex align-items-center">
+                  <i
+                    className={`fas fa-user-circle text-light ${!minimized ? "mr-2" : null}`}
+                    style={{ fontSize: "30px" }}></i>
+                  <CSSTransition in={!minimized} timeout={50} classNames="fade" appear>
+                    <span>{currentUser.name}</span>
+                  </CSSTransition>
                 </span>
-              ))}
+              </div>
+              <hr className="mb-0 w-100" style={{ background: "rgba(0,0,0,0.2)" }} />
+              <div className="px-2 py-2">
+                {links.map(link => (
+                  <span key={cuid()}>
+                    <NavLink
+                      exact={link.exact}
+                      to={link.path}
+                      className={`${link.className} d-flex mb-1  justify-content-between text-nowrap`}
+                      activeClassName="bg-info active shadow"
+                      onClick={() => this.toggleDropDown(link.id)}>
+                      <span>
+                        <i className={link.icon}></i>
+                        {!minimized ? (
+                          <CSSTransition in={!minimized} timeout={50} classNames="fade">
+                            <span className={link.textClass}>{link.text}</span>
+                          </CSSTransition>
+                        ) : null}
+                      </span>
+
+                      {link.dropdown && !minimized ? (
+                        link.status === "open" ? (
+                          <span>
+                            <i className="fas fa-chevron-down"></i>
+                          </span>
+                        ) : (
+                          <span>
+                            <i className="fas fa-chevron-left"></i>
+                          </span>
+                        )
+                      ) : null}
+                    </NavLink>
+                    {link.dropdown && link.status === "open" && !minimized
+                      ? link.subLinks.map(subLink => (
+                          <NavLink
+                            to={subLink.path}
+                            className={`${subLink.className} mt-1`}
+                            style={{ backgroundColor: "rgba(0,0,0,0.3)" }}
+                            key={cuid()}
+                            activeClassName="bg-light text-dark active shadow">
+                            <i className={`fas fa-chevron-right mr-3`}></i>
+                            <i className={`${subLink.icon} mr-1`}></i>
+                            {subLink.text}
+                          </NavLink>
+                        ))
+                      : null}
+                  </span>
+                ))}
+              </div>
+            </div>
+            <div className="d-flex align-self-end w-100 justify-content-center flex-wrap mt-auto mb-0">
+              <hr className="mb-0 w-100" style={{ background: "rgba(0,0,0,0.2)" }} />
+              <button
+                className="btn btn-transparent btn-lg text-light shadow-none"
+                onClick={this.toggle}>
+                <i
+                  className={
+                    minimized
+                      ? "fad fa-chevron-circle-right display-4"
+                      : "fad fa-chevron-circle-left display-4"
+                  }
+                  style={{ fontSize: "30px" }}></i>
+              </button>
             </div>
           </nav>
-          <div
-            className="d-flex w-100 justify-content-center sticky-top"
-            style={{ height: 'fill' }}>
-            <button
-              className="btn btn-transparent btn-lg text-light"
-              onClick={this.toggle}>
-              <i
-                className={
-                  minimized
-                    ? 'fad fa-chevron-circle-right display-4'
-                    : 'fad fa-chevron-circle-left display-4'
-                }
-                style={{ fontSize: '30px' }}></i>
-            </button>
-          </div>
         </div>
       </CSSTransition>
     );

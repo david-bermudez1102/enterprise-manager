d6be64da7a431d5365ad3182203eba4cdc226a13
Juan D. Bermudez <juan.bermudez1102@outlook.com>
2020-03-20 22:53:04 -0400

    Added wraper for icons and out css classes in object

--- enterprise-manager-frontend/src/components/Accounts/Account.js
+++ enterprise-manager-frontend/src/components/Accounts/Account.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Avatar from "../Home/SideBar/Avatar";
 
 const capitalize = require("capitalize");
 
@@ -7,19 +8,30 @@ const icons = [
   { type: "Manager", className: "fas fa-user-shield" },
   { type: "Employee", className: "fas fa-user" }
 ];
+
 const Account = ({ account }) => {
   const icon = icons.find(icon => icon.type === account.type);
   return (
-    <div
-      className="row border-0 shadow-sm rounded list-group-item py-md-3 py-sm-2 mb-1 d-flex align-items-center justify-content-between display-4 list-group-item-action px-4"
-      style={{ fontSize: "20px", cursor: "pointer" }}>
-      <span className="order-sm-2 order-md-1 py-3 col-sm-12 col-xl-7 col-lg-8 col-md-8">
-        <i className={`${icon.className} text-primary`} title="Employee"></i>{" "}
+    <div className={classNames.listItem} style={{ fontSize: "22px", cursor: "pointer" }}>
+      <span className={classNames.firstCol}>
+        <Avatar currentUser={account} style={{ zIndex: 1 }} size={50} />
+        <div
+          id="roleIconWrapper"
+          title={icon.type}
+          className={classNames.roleIconWrapper}
+          style={{
+            zIndex: 2,
+            width: "30px",
+            height: "30px",
+            fontSize: "14px",
+            background: "rgba(0,0,0,0.6)",
+            marginTop: "30px"
+          }}>
+          <i className={icon.className}></i>
+        </div>
         {capitalize.words(account.name)}
       </span>
-      <span
-        className="order-sm-1 order-md-2 py-3 col-sm-12 col-xl-5 col-lg-4 col-md-4 d-flex justify-content-between text-primary"
-        style={{ fontSize: "18px" }}>
+      <span className={classNames.secondCol} style={{ fontSize: "18px" }}>
         <i className="fas fa-envelope"></i>
         <i className="fas fa-power-off"></i>
         <i className="fas fa-unlock"></i>
@@ -28,6 +40,17 @@ const Account = ({ account }) => {
       </span>
     </div>
   );
+};
+
+const classNames = {
+  listItem:
+    "row border-0 shadow-sm rounded list-group-item py-md-3 py-sm-2 mb-1 d-flex align-items-center justify-content-between display-4 list-group-item-action px-4",
+  firstCol:
+    "d-flex order-sm-2 order-md-1 py-3 col-sm-12 col-xl-7 col-lg-8 col-md-8 align-items-center",
+  secondCol:
+    "order-sm-1 order-md-2 py-3 col-sm-12 col-xl-5 col-lg-4 col-md-4 d-flex justify-content-between text-primary",
+  roleIconWrapper:
+    "rounded-circle shadow text-light ml-n4 d-flex align-items-center justify-content-center"
 };
 
 export default Account;

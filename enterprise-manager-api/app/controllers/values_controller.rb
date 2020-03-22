75b3ddffb91ac2f9c2dcc562c07997cc565a8d3f
Juan D. Bermudez <juan.bermudez1102@outlook.com>
2020-03-21 23:31:59 -0400

    Adde callbacks and create action

--- enterprise-manager-api/app/controllers/values_controller.rb
+++ enterprise-manager-api/app/controllers/values_controller.rb
@@ -1,6 +1,8 @@
 class ValuesController < ApplicationController
   before_action :set_organization
   before_action :set_form
+  before_action :set_record_field, only: :create
+  before_action :set_record, only: :create
   before_action :set_value, only: %i[show, update]
 
   def index
@@ -12,9 +14,18 @@ class ValuesController < ApplicationController
     render json: ValueSerializer.new(@value)
   end
 
+  def create
+    value = @record.values.build(value_params)
+    if !@record_field.nil? && value.save
+      render json: ValueSerializer.new(value, messages: ["Record saved successfully."])
+    else
+      render json: { errors: value.errors.full_messages }
+    end
+  end
+
   def update
     if @value.update(value_params)
-      render json: ValueSerializer.new(@value)
+      render json: ValueSerializer.new(@value, messages: ["Record saved successfully."])
     else
       render json: { errors: @value.errors.full_messages }
     end
@@ -23,7 +34,7 @@ class ValuesController < ApplicationController
   private
 
   def value_params
-    params.require(:value).permit(:content)
+    params.require(:value).permit(:content, :record_id, :record_field_id)
   end
 
   def set_organization
@@ -32,6 +43,14 @@ class ValuesController < ApplicationController
 
   def set_form
     @form = @organization.forms.find_by(id: params[:form_id])
+  end
+
+  def set_record_field
+    @record_field = @form.record_fields.find_by(id: params[:record_field_id])
+  end
+
+  def set_record
+    @record = @form.records.find_by(id: params[:record_id])
   end
 
   def set_value

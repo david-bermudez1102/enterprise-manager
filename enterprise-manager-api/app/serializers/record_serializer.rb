96230221282ef7b4d93f9c40aaf3b51927b63b43
Juan D. Bermudez <juan.bermudez1102@outlook.com>
2020-08-10 21:17:24 -0400

    Optimized serializer speed by not calling value serializer

--- enterprise-manager-api/app/serializers/record_serializer.rb
+++ enterprise-manager-api/app/serializers/record_serializer.rb
@@ -19,21 +19,35 @@ class RecordSerializer
 
 
   link :values do |object|
+    self.normalize_values(object)
+  end
+
+  link :versions do |obj, params|
+    if params[:with_versions]
+      versions = obj.values.map(&:versions).flatten
+      versions << obj.zoho_integration_record.versions if obj.zoho_integration_record
+      versions << obj.quickbooks_integration_record.versions if obj.quickbooks_integration_record
+      versions.sort { |a,b| b <=> a }
+
+    end
+  end
+
+  def self.normalize_values(record)
     new_hash = {}
-    object.values.each do |value|
+    record.values.each do |value|
       
-      new_hash[:id] = object.id
-      new_hash[:formId] = object.form_id
+      new_hash[:id] = record.id
+      new_hash[:formId] = record.form_id
       new_hash[value.record_field_id] = value.content
       new_hash[value.record_field_id] = value.content_after_dependents if value.content_after_dependents
       new_hash[value.record_field_name] = value.zoho_api_content
       
     end
-    new_hash[:createdBy] = object.account.name if object.account 
-      new_hash[:createdAt] = object.created_at
-      new_hash[:updatedAt] = object.updated_at
-      new_hash[:zohoRecordId] = object.zoho_integration_record.external_id if object.zoho_integration_record
-      new_hash[:quickbooksRecordId] = object.quickbooks_integration_record.external_id if object.quickbooks_integration_record
+    new_hash[:createdBy] = record.account.name if record.account 
+      new_hash[:createdAt] = record.created_at
+      new_hash[:updatedAt] = record.updated_at
+      new_hash[:zohoRecordId] = record.zoho_integration_record.external_id if record.zoho_integration_record
+      new_hash[:quickbooksRecordId] = record.quickbooks_integration_record.external_id if record.quickbooks_integration_record
     new_hash
   end
 
